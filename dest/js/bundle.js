44864a2c5d424059a722d7c0349d6c3e61cc56a5
Oleksii Shnepov <shnax0210@gmail.com>
2021-07-03 23:45:31 +0300

    Unified editors to single one, added approach for examples

--- dest/js/bundle.js
+++ dest/js/bundle.js
@@ -49212,36 +49212,35 @@ module.exports = zipObject;
 
 },{"./_assignValue":135,"./_baseZipObject":190}],350:[function(require,module,exports){
 const buildStates = require('./states-builder').buildStates;
-const selectionsTypes = require('./states-builder').selectionsTypes;
 const buildTransactions = require('./transactions-builder').buildTransactions;
 const showStateMachineGraph = require('./state-machine-graph').showStateMachineGraph;
 const markStates = require('./states-marker').markStates;
 const _ = require('lodash');
 
-function createStateMachine(buildStatesDescription, stateMarker, eventsInfo) {
+function createStateMachine(stateMachineDefinition) {
     function evaluateStates() {
-        if (!buildStatesDescription || !_.isFunction(buildStatesDescription)) {
-            console.warn("Build states description function is not defined or is not a function:" + buildStatesDescription);
+        if (!stateMachineDefinition.statesDescription) {
+            console.warn("There is not statesDescription: " + JSON.stringify(stateMachineDefinition));
             return {};
         }
 
-        let states = buildStates(buildStatesDescription());
+        let states = buildStates(stateMachineDefinition.statesDescription);
 
-        if (!stateMarker || !_.isFunction(stateMarker.markState)) {
-            console.warn("State marker is not defined or defined in wrong way:" + stateMarker);
+        if (!_.isFunction(stateMachineDefinition.markState)) {
+            console.warn("Mark state function is missing or is not a function: " + JSON.stringify(stateMachineDefinition));
             return states;
         }
 
-        return markStates(states, stateMarker.possibleMarks, stateMarker.markState);
+        return markStates(states, stateMachineDefinition.marks, stateMachineDefinition.markState);
     }
 
     function evaluateTransactions(states) {
-        if (!eventsInfo || !eventsInfo.events) {
-            console.warn("Event builder is not defined or defined in wrong way:" + eventsInfo);
+        if (!stateMachineDefinition.events) {
+            console.warn("There is no events: " + JSON.stringify(stateMachineDefinition));
             return [];
         }
 
-        return buildTransactions(eventsInfo.events, states, eventsInfo.marks)
+        return buildTransactions(stateMachineDefinition.events, states, stateMachineDefinition.marksThatEventsWillBeAppliedTo)
     }
 
     const stateMachine = {};
---
+++ examples.js
@@ -0,0 +1,93 @@
+const examples = [{
+   name: "Users buy products",
+   code: `function registerUser(state, id) {
+   if (Array.from(state.users).map(user => user.id).includes(id)) {
+       return;
+   }
+   
+   state.users.add({id: id, products: new Set()})
+}
+   
+function buyProduct(state, userId, productId) {
+   const users = Array.from(state.users);
+   if (!users.map(user => user.id).includes(userId)) {
+       return;
+   }
+   
+   users.find(user => user.id === userId).products.add(productId);
+}
+    
+return {
+    statesDescription: {
+        users: {
+            selectionType: "ANY_COMBINATION_OF",
+            description: {
+                id: {
+                    selectionType: "ANY_OF",
+                    unique: true,
+                    values: ["user1", "user2"]
+                },
+                products: {
+                    selectionType: "ANY_COMBINATION_OF",
+                    values: ["product1", "product2"]
+                }
+            }
+        }
+   },
+   marks: [{
+            label: "Valid",
+            color: "lime"
+        },
+        {
+            label: "NotValid",
+            color: "red"
+        }
+   ],
+   markState: function(state) {
+       if (state.id === 10) {
+            return "NotValid"
+       }
+   
+       return "Valid";
+   },
+   marksThatEventsWillBeAppliedTo: ["Valid"],
+   events: [
+       {
+            name: "RegistrationUser1",
+            handle: function (state) {
+                registerUser(state, "user1");
+            }
+       },
+       {
+            name: "RegistrationUser2",
+            handle: function (state) {
+                registerUser(state, "user2");
+            }
+       },
+       {
+            name: "User1BuyProduct1",
+            handle: function (state) {
+                buyProduct(state, "user1", "product1");
+            }
+       },
+       {
+            name: "User1BuyProduct2",
+            handle: function (state) {
+                buyProduct(state, "user1", "product2");
+            }
+       },
+       {
+            name: "User2BuyProduct1",
+            handle: function (state) {
+                buyProduct(state, "user2", "product1");
+            }
+       },
+       {
+            name: "User2BuyProduct2",
+            handle: function (state) {
+                buyProduct(state, "user2", "product2");
+            }
+       }
+   ]
+}`
+}]

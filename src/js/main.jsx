e3f8c7c001df879c2c4516d5b32f24c802f8ec10
Oleksii Shnepov <shnax0210@gmail.com>
2021-07-24 18:56:57 +0300

    Added routers for tabs

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,5 +1,11 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+
+const BrowserRouter = require('react-router-dom').BrowserRouter;
+const Switch = require('react-router-dom').Switch;
+const Route = require('react-router-dom').Route;
+const Link = require('react-router-dom').Link;
+
 const styled = require('styled-components').default;
 
 const Editor = require('./state-machine-editor-component.jsx').Editor;
@@ -10,13 +16,13 @@ const List = styled.ul`
     flex-wrap: nowrap;
     list-style-type: none;
     padding: 0;
+    margin: 0 0;
 `
 
 const ListItem = styled.li`
-    width: 100%;
-    height: 30px;
+    width: 150px;
+    height: 25px;
     background-color: #f1f1f1;
-    border-radius: 12px;
     text-align: center;
     vertical-align: middle;
 `
@@ -25,37 +31,67 @@ const SelectedListItem = styled(ListItem)`
     background-color: #808080;
 `
 
-const ListLink = styled.a`
-    padding: 14px 16px;
+const ListLink = styled(Link)`
     text-align: center;
+    text-decoration: none;
+    display: block;
+    height: 100%;
+    width: 100%;
+    color: black;
+    $:visited, $:hover, $:active {
+        color: inherit;
+    }
+}
+`
+
+const Line = styled.div`
+    width: 100%;
+    height: 2px;
+    background-color: #808080;
 `
 
 class Tabs extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {selectedTab: this.props.tabs[0]};
-    }
-
-    selectTab(tab) {
-        this.setState({selectedTab: tab});
-    }
-
     render() {
-        const selectedTab = this.state.selectedTab;
-        const SelectedTabComponent = this.state.selectedTab.component;
-
-        const list = this.props.tabs.map(tab => {
-            const ListItemElement = tab.name === selectedTab.name ? SelectedListItem : ListItem;
-            return (
-                <ListItemElement onClick={() => this.selectTab(tab)} key={tab.name}>
-                    <ListLink>{tab.name}</ListLink>
-                </ListItemElement>)
+        const tabs = this.props.tabs.map(tab => {
+            const TabLink = () => {
+                return <ListLink to={tab.path}>{tab.name}</ListLink>
+            }
+            
+            return (<Switch key={tab.name}>
+                <Route path={tab.path}>
+                    <SelectedListItem><TabLink/></SelectedListItem>
+                </Route>
+                <Route>
+                    <ListItem><TabLink/></ListItem>
+                </Route>
+            </Switch>);
         });
 
+        const TabContent = () => {
+            const tabRouters = this.props.tabs.map(tab => {
+                const TabComponent = tab.component
+                return (
+                    <Route path={tab.path} key={tab.path}>
+                        <TabComponent/>
+                    </Route>)
+            });
+            
+            const DefaultTabRouter = () => {
+                const FirstTabContent = this.props.tabs[0].component;
+                return <Route><FirstTabContent/></Route>;
+            }
+
+            return (<Switch>
+                {tabRouters}
+                <DefaultTabRouter/>
+            </Switch>);
+        }
+        
         return (
             <div>
-                <List>{list}</List>
-                <SelectedTabComponent isCodeSavingEnabled={true}/>
+                <List>{tabs}</List>
+                <Line/>
+                <TabContent/>
             </div>
         );
     }
@@ -64,6 +100,7 @@ class Tabs extends React.Component {
 
 const exampleTabs = [{
         name: "Code area",
+        path: "/code-area",
         component: () => {
             return (
                 <div>
@@ -75,10 +112,12 @@ const exampleTabs = [{
     },
     {
         name: "Examples",
+        path: "/examples",
         component: () => <Examples/>
     },
     {
         name: "Documentation",
+        path: "/documentation",
         component: () => <h1>Hello</h1>
     }]
 
@@ -95,14 +134,13 @@ const IntroductionAreaDiv = styled.div`
 
 const application = (
     <ApplicationDiv>
-        <IntroductionAreaDiv>
-            <h1>State machine analyzer</h1>
-        </IntroductionAreaDiv>
-        <Tabs tabs={exampleTabs}/>
+        <BrowserRouter>
+            <IntroductionAreaDiv>
+                <h1>State machine analyzer</h1>
+            </IntroductionAreaDiv>
+            <Tabs tabs={exampleTabs}/>
+        </BrowserRouter>
     </ApplicationDiv>
 );
 
-ReactDOM.render(
-    application,
-    document.getElementById('application')
-);
+ReactDOM.render(application, document.getElementById('application'));
